fd943989565a16e19195c5542595b17e8c135d1a
miloujb <emilybailey@Emilys-MacBook-Air.local>
2021-04-08 09:27:54 +0100

    hello world

---
+++ src/Components/Tracks.js
@@ -0,0 +1,13 @@
+import React, {useState, useEffect} from 'react';
+import Plot from "react-plotly.js"
+import axios from 'axios'
+
+const Tracks = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Tracks;
